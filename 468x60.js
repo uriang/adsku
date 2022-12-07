c0e2fadf9471873f77afc78ca5d58c12db83b07f
uriang <88535206+uriang@users.noreply.github.com>
2022-12-07 12:16:56 +0800

    Create 468x60.js

---
+++ 468x60.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : 'd4c24966e9f0c267086ca703c1e76f42',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/d4c24966e9f0c267086ca703c1e76f42/invoke.js"></scr' + 'ipt>');
